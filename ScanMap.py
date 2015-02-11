4a562a6bbd512a7d09e32ce4dfb34749d69f62d1
Vienna <vienna@nionusers.com>
2015-02-11 20:52:35 +0100

    made it run (mittelberger)

--- ScanMap.py
+++ ScanMap.py
@@ -1,9 +1,10 @@
 # standard libraries
 import gettext
 import logging
-
+import numpy as np
 # third party libraries
 # None
+import ViennaTools.ViennaTools as vt
 
 # local libraries
 from nion.swift import Panel
@@ -11,8 +12,10 @@ from nion.swift import Workspace
 from nion.swift.model import DataItem
 from nion.ui import Binding
 
+
 _ = gettext.gettext
 
+coord_dict = {'top-left': None, 'top-right': None, 'bottom-right': None, 'bottom-left': None}
 
 class ScanMap(Panel.Panel):
 
@@ -38,24 +41,30 @@ class ScanMap(Panel.Panel):
 
         bottom_button_row = ui.create_row_widget()
         top_button_row = ui.create_row_widget()
+        done_button_row = ui.create_row_widget()
         tl_button = ui.create_push_button_widget(_("Top Left"))
         tr_button = ui.create_push_button_widget(_("Top Right"))
         bl_button = ui.create_push_button_widget(_("Bottom Left"))
         br_button = ui.create_push_button_widget(_("Bottom Right"))
+        done_button = ui.create_push_button_widget(_("Done"))
 
         def tl_button_clicked():
-            logging.info("TODO: load function to save top left")
+            save_coords('top-left')
         def tr_button_clicked():
-            logging.info("TODO: load function to save top right")
+            save_coords('top-right')
         def bl_button_clicked():
-            logging.info("TODO: load function to save bottom left")
+            save_coords('bottom-left')
         def br_button_clicked():
-            logging.info("TODO: load function to save bottom right")
+            save_coords('bottom-right')
+        def done_button_clicked():
+            if not None in coord_dict.viewvalues():
+                vt.SuperScan_mapping(coord_dict, imsize=200)
 
         tl_button.on_clicked = tl_button_clicked
         tr_button.on_clicked = tr_button_clicked
         bl_button.on_clicked = bl_button_clicked
         br_button.on_clicked = br_button_clicked
+        done_button.on_clicked = done_button_clicked
 
         bottom_button_row.add(tl_button)
         top_button_row.add_spacing(8)
@@ -64,12 +73,16 @@ class ScanMap(Panel.Panel):
         top_button_row.add(bl_button)
         top_button_row.add_spacing(8)
         top_button_row.add(br_button)
+        
+        done_button_row.add(done_button)
 
         column.add(edit_row)
         column.add_spacing(8)
         column.add(bottom_button_row)
         column.add_spacing(8)
         column.add(top_button_row)
+        column.add_spacing(8)
+        column.add(done_button_row)        
         column.add_stretch()
 
         self.widget = column
@@ -77,3 +90,8 @@ class ScanMap(Panel.Panel):
 
 workspace_manager = Workspace.WorkspaceManager()
 workspace_manager.register_panel(ScanMap, "scanmap-panel", _("Scan a Map"), ["left", "right"], "right" )
+
+def save_coords(position):
+    global coord_dict
+    coord_dict[position] = (vt.as2_get_control('StageOutX'), vt.as2_get_control('StageOutY'), vt.as2_get_control('StageOutZ'), vt.as2_get_control('EHTFocus'))
+    
