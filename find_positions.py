Fixed error in creation of map_coordinates and removed double radian converation of scan rotation angle

find_positions.py
@@ -38,25 +38,25 @@ import tifffile
 
 if __name__=='__main__':
     
-    dirpath = '/3tb/maps_data/map_2015_04_15_13_13/'
+    dirpath = '/3tb/maps_data/map_2015_09_04_13_04/'
     
-    overview = '//3tb/maps_data/map_2015_04_15_13_13/overview.tif'
+    overview = '/3tb/maps_data/map_2015_09_04_13_04/Overview_464.0_nm.tif'
     
-    size_overview = 1024 #nm
-    size_frames = 12 #nm
+    size_overview = 464 #nm
+    size_frames = 20 #nm
     #number of frames in x- and y-direction
-    number_frames = (22,22)
+    number_frames = (10,10)
     #borders of the area where the first frame is expected (in % of the overview image)
     #has to be a tuple of the form (lower-y, higher-y, lower-x, higher-x)
-    area_first_frame = (2,50,10,80) #%
+    area_first_frame = (15,25,15,25) #%
     #enter search area for the following images. Search is always performed around the position of the last image.
     #tuple hast to have the form (negtive-y, positive-y, negative-x, positive-x)
-    tolerance_within_rows = (0.8,1.2,-0.5,4.8)
-    tolerance_within_columns = (-0.8,4,0.1,1)
+    tolerance_within_rows = (20,20,20,20)
+    tolerance_within_columns = (20,20,20,20)
     #Enter the number of frames that should be included here. Type None to use all frames.
-    number_frames_included = 22
+    number_frames_included = -1
     #
-    bad_correlation_threshold = 0.5
+    bad_correlation_threshold = 0
     
     
     
@@ -101,6 +101,7 @@ if __name__=='__main__':
     shape_im = np.shape(im)
     scale = (size_frames/float(shape_im[0])/(size_overview/float(shape_over[0])))
     im = cv2.resize(im, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+    #im = cv2.GaussianBlur(im, None, 3)
     result = cv2.matchTemplate(over[int(area_first_frame[0]*shape_over[0]):int(area_first_frame[1]*shape_over[0]),
                                     int(area_first_frame[2]*shape_over[1]):int(area_first_frame[3]*shape_over[1])],
                                     im, method=cv2.TM_CCOEFF_NORMED)
@@ -112,6 +113,9 @@ if __name__=='__main__':
     added[1,maxi[0]:maxi[0]+im.shape[0], maxi[1]:maxi[1]+im.shape[1]] += im
     cv2.putText(added[0], str(int(name[0:4])), (maxi[1]-4,maxi[0]-2), cv2.FONT_HERSHEY_PLAIN, 2, color, thickness=2)
     
+    if number_frames_included < 1:
+        number_frames_included = None
+        
     counter = 0
     for name in matched_frames[1:number_frames_included]:
         im = np.array(cv2.imread(dirpath+name, -1))#*3
@@ -136,7 +140,7 @@ if __name__=='__main__':
         if int(name[0:4])%number_frames[0] == 0 and int(name[0:4]) != 0:
             in_column_distance_list.append(np.array(maxi) - np.array(position_list[counter-number_frames[0]+1][1:]))
         else:
-            if corr_max < bad_correlation_threshold:
+            if corr_max < bad_correlation_threshold and len(in_row_distance_list) > 1:
                 maxi = (position_list[counter][1] + int(np.median(np.array(in_row_distance_list)[:,0])),
                         position_list[counter][2] + int(np.median(np.array(in_row_distance_list)[:,1])))
             in_row_distance_list.append(np.array(maxi) - np.array(position_list[counter][1:]))

maptools/autotune.py
@@ -155,7 +155,7 @@ def create_record_parameters(superscan, frame_parameters, detectors={'HAADF': Fa
             parameters['fov_nm'] = frame_parameters['fov']
         
         if frame_parameters.get('rotation') is not None:
-            parameters['rotation_rad'] = frame_parameters['rotation']*np.pi/180.0
+            parameters['rotation_rad'] = frame_parameters['rotation']
     else:
         parameters = None
         

maptools/mapper.py
@@ -174,8 +174,8 @@ def find_nearest_neighbors(number, target, points):
 def create_map_coordinates(leftX, topY, imsize, distance, num_subframes, coords, compensate_stage_error=False):
     extra_lines = 2  # Number of lines to add at the beginning of the map
     extra_frames = 5  # Number of extra moves at each end of a line
-    oldm = 1.5  # odd line distance multiplicator (adjust odd lines offset)
-    eldm = 1.0  # even line distance multiplicator (adjust even lines offset)
+    oldm = 0.5  # odd line distance multiplicator (adjust odd lines offset)
+    eldm = 0  # even line distance multiplicator (adjust even lines offset)
 
     num_subframes = np.array(num_subframes)
 
@@ -200,9 +200,10 @@ def create_map_coordinates(leftX, topY, imsize, distance, num_subframes, coords,
     for j in range(num_subframes[1]):
         for i in range(num_subframes[0]):
             if j % 2 == 0:  # Odd lines (have even indices because numbering starts with 0), e.g. map from left to right
-                map_coords.append( tuple( ( leftX+i*(imsize+distance),  topY-j*(imsize+oldm*distance) ) ) +
-                                   tuple( interpolation((leftX+i*(imsize+distance),
-                                                         topY-j*(imsize+oldm*distance)), coords) ) )
+                map_coords.append( tuple( ( leftX+i*(imsize+distance),  topY-j*(imsize+distance)-oldm*(imsize+distance) ) ) +
+                                   tuple( interpolation( (leftX+i*(imsize+distance),
+                                                          topY-j*(imsize+distance)-oldm*(imsize+distance) ),
+                                                          coords) ) )
                 
                 # Apply correct (continuous) frame numbering for all cases. If no extra positions are added, just append
                 # the correct frame number. Elsewise append the correct frame number if a non-additional one, else None
@@ -214,9 +215,10 @@ def create_map_coordinates(leftX, topY, imsize, distance, num_subframes, coords,
                     frame_number.append(None)
 
             else: # Even lines, e.g. scan from right to left
-                map_coords.append( tuple( (leftX+(num_subframes[0]-(i+1))*(imsize+distance), topY-j*(imsize+eldm*distance)) ) +
+                map_coords.append( tuple( (leftX+(num_subframes[0]-(i+1))*(imsize+distance), topY-j*(imsize+distance) - eldm*(imsize+distance) ) ) +
                                    tuple( interpolation( (leftX+(num_subframes[0]-(i+1))*(imsize+distance),
-                                                          topY-j*(imsize+eldm*distance)), coords) ) )
+                                                          topY-j*(imsize+eldm*distance) - eldm*(imsize+distance) ),
+                                                          coords) ) )
                 
                 # Apply correct (continuous) frame numbering for all cases. If no extra positions are added, just append
                 # the correct frame number. Elsewise append the correct frame number if a non-additional one, else None
@@ -618,7 +620,8 @@ def SuperScan_mapping(coord_dict, filepath='Z:\\ScanMap\\', do_autofocus=False, 
                         test_map.append(frame_coord)
 
         else:
-            test_map.append(frame_coord)
+            if frame_number[counter-1] is not None:
+                test_map.append(frame_coord)
 
     if do_autofocus and autofocus_pattern == 'testing':
         bad_frames_file = open(store+'bad_frames.txt', 'w')
@@ -642,7 +645,7 @@ def SuperScan_mapping(coord_dict, filepath='Z:\\ScanMap\\', do_autofocus=False, 
         vt.as2_set_control(as2, 'StageOutY', map_center[1])
         time.sleep(5)
         #acquire image and save it
-        overview_parameters = {'size_pixels': (4096, 4096), 'center': (0,0), 'pixeltime': 2, \
+        overview_parameters = {'size_pixels': (4096, 4096), 'center': (0,0), 'pixeltime': 4, \
                             'fov': over_size, 'rotation': rotation}
         image = autotune.image_grabber(superscan=superscan, frame_parameters=overview_parameters)
 