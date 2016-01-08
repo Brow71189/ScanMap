08e65e36d8e1e6f12e03440eb6ad302fcced93c9
Andreas <andreas.mittelberger@univie.ac.at>
2016-01-08 12:58:01 +0100

    Fixed error when converting int to str implicitly

--- maptools/mapper.py
+++ maptools/mapper.py
@@ -620,7 +620,7 @@ class Mapping(object):
                                     self.frame_parameters['pixeltime'] = pixeltimes[i]
                                 data = img.image_grabber(frame_parameters=self.frame_parameters)
                                 splitname = os.path.splitext(name)
-                                name = splitname[0] + ('_{:0'+len(str(self.number_of_images))+'d}'
+                                name = splitname[0] + ('_{:0'+str(len(str(self.number_of_images)))+'d}'
                                                        ).format(i) + splitname[1]
                                 tifffile.imsave(os.path.join(self.store, name), data)
                             if self.switches.get('blank_beam'):
