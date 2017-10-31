11f922fc0c7fe9825617457faa29b752b70fd98e
andreas <andreas.mittelberger@univie.ac.at>
2017-10-31 09:26:55 +0100

    latest version of electron counting

--- c_electron_counting.pyx
+++ c_electron_counting.pyx
@@ -12,10 +12,12 @@ from libc.math cimport round, atan, log, exp, pow, sqrt
 from libc.stdlib cimport rand, RAND_MAX
 
 DEF factorials_table_length = 20
+DEF integration_range_gamma = 2.0
+DEF pi = 3.14159
 
 cdef long factorials_table[factorials_table_length]
 
-def electron_counting(image, baseline=0.002, countlevel=0.01, peakwidth=1):
+def electron_counting(image, baseline=0.002, countlevel=0.01, peakwidth=1, only_integrate=False):
     cdef float[:, :] c_image = image
     result = numpy.zeros(image.shape, dtype=numpy.float32)
     cdef float[:, :] c_result = result
@@ -24,11 +26,23 @@ def electron_counting(image, baseline=0.002, countlevel=0.01, peakwidth=1):
     cdef float c_peakwidth = peakwidth
     cdef int c_height = image.shape[0]
     cdef int c_width = image.shape[1]
-    c_electron_counting(c_image, c_result, c_height, c_width, c_baseline, c_countlevel, c_peakwidth)
+    cdef bint c_only_integrate = only_integrate
+    c_electron_counting(c_image, c_result, c_height, c_width, c_baseline, c_countlevel, c_peakwidth, c_only_integrate)
+    return result
+
+def find_most_likely_counts(image, baseline=0.0, countlevel=0.1):
+    cdef float[:, :] c_image = image
+    result = numpy.zeros(image.shape, dtype=numpy.float32)
+    cdef float[:, :] c_result = result
+    cdef float c_baseline = baseline
+    cdef float c_countlevel = countlevel
+    cdef int c_height = image.shape[0]
+    cdef int c_width = image.shape[1]
+    c_find_most_likely_counts_image(image, c_result, c_height, c_width, c_baseline, c_countlevel)
     return result
 
 cdef void c_electron_counting(float[:, :] image, float[:, :] result, int height, int width, float baseline,
-                              float countlevel, float peakwidth):
+                              float countlevel, float peakwidth, bint only_integrate):
     global factorials_table
     cdef int k, i
     cdef float integral
@@ -36,20 +50,15 @@ cdef void c_electron_counting(float[:, :] image, float[:, :] result, int height,
     cdef int randnum
     cdef float threshold
     cdef float counts_divisor
-    cdef unsigned short integration_range
-    cdef unsigned short integration_range_gamma
-    cdef float divided_counts
-    cdef float rounded_counts
-    #cdef float image_mean
-    #image_mean = 0
+    cdef float peak_max
 
     create_factorials_table(factorials_table_length)
-    integration_range_gamma = 4
-    threshold = countlevel/(1.0+pow(integration_range_gamma, 2)) # height of our peak at +- integration_range_gamma
+    # All following calculations are done with the height of the Lorentzians normalized to 1
+    threshold = 1/(1.0+pow(integration_range_gamma, 2)) # height of our peak at +- integration_range_gamma
     # Integral of our peak from -integration_range_gamma to +integration_range_gamma
-    counts_divisor = countlevel*peakwidth*(atan(<float>integration_range_gamma)-atan(-1.0*<float>integration_range_gamma))
+    counts_divisor = countlevel*peakwidth*(atan(integration_range_gamma)-atan(-1.0*integration_range_gamma))
     # Integration range going from -integration_range_gamma to +integration_range_gamma
-    peaklength = <int>(round(2.0*<float>integration_range_gamma*peakwidth))
+    peaklength = <int>(round(2.0*integration_range_gamma*peakwidth))
     # correct integral for baseline
     counts_divisor -= baseline*(<float>peaklength)
 
@@ -57,46 +66,55 @@ cdef void c_electron_counting(float[:, :] image, float[:, :] result, int height,
         integral = 0
         index = -1
         i = 0
+        peak_max = countlevel
         while i < width:
-            if index != -1 and (image[k, i] - baseline < threshold or i - index >= peaklength or i == width-1):
-                divided_counts = integral/counts_divisor
-                result[k, index] = find_most_likely_counts(divided_counts) #round(divided_counts)
-                #image_mean += divided_counts
+            if index != -1 and (image[k, i] - baseline <= peak_max*threshold or i - index >= peaklength or i == width-1):
+                if only_integrate:
+                    result[k, index] = integral
+                else:
+                    result[k, index] = c_find_most_likely_counts(integral/counts_divisor)
                 integral = 0
-                if image[k, i] - baseline >= threshold:
+                if image[k, i] - baseline > peak_max*threshold:
                     randnum = <int>(<float>rand() / <float>RAND_MAX * <float>peaklength)
                     i = index + 1 + randnum
                     index = i
                     integral = image[k, i]
                 else:
                     index = -1
+                peak_max = countlevel
+
             elif index != -1:
                 integral += image[k, i] - baseline
+                if image[k, i] > peak_max:
+                    peak_max = image[k, i]
 
-            if index == -1 and image[k, i] - baseline >= threshold:
+            if index == -1 and image[k, i] - baseline > peak_max*threshold:
                 index = i
                 integral = image[k, i]
 
             i += 1
 
-#    image_mean /= <float>(width*height)
-#    for k in range(height):
-#        for i in range(width):
-#            result[k, i] = find_most_likely_counts(result[k, i], image_mean)
+cdef void c_find_most_likely_counts_image(float[:, :] image, float[:, :] result, int height, int width,
+                                          float baseline, float countlevel):
+    cdef int k, i
+    create_factorials_table(factorials_table_length)
+    for k in range(height):
+        for i in range(width):
+            result[k, i] = c_find_most_likely_counts((image[k ,i]-baseline)/countlevel)
 
-cdef float count_probability(int counts, float x):
-    return ((1/sqrt(2.0*3.14159*(<float>counts)**2)*exp(-1.0*pow(x - <float>counts, 2.0)/(2.0*(<float>counts)**2))) *
+cdef float c_count_probability(int counts, float x):
+    return ((1/sqrt(2.0*pi*(<float>counts)**2)*exp(-1.0*pow(x - <float>counts, 2.0)/(2.0*(<float>counts)**2))) *
             (pow((<float>counts), <float>counts)*exp(-1.0*(<float>counts))/(<float>factorials_table[counts])))
 
-cdef float find_most_likely_counts(float x):
-    if round(x) == 0.0:
+cdef float c_find_most_likely_counts(float x):
+    if round(x+0.25) == 0.0:
         return 0.0
     cdef int counter
     cdef float max_probability, current_probability
     max_probability = 0
     counter = 0
     while counter < factorials_table_length:
-        current_probability = count_probability(counter, x)
+        current_probability = c_count_probability(counter, x)
         if current_probability < max_probability:
             return counter - 1
         max_probability = current_probability
--- subframes.py
+++ subframes.py
@@ -25,32 +25,37 @@ try:
 except:
     from .maptools import autotune as at
 import pyximport; pyximport.install()
-import c_electron_counting
+try:
+    import c_electron_counting
+except:
+    from . import c_electron_counting
 
 #######################################################################################################################
 #######################################################################################################################
 #######################################################################################################################
-dirpath = '/3tb/maps_data/map_2015_04_15_13_13'
-imsize = 12
-graphene_threshold = -1
+dirpath = '/3tb/maps_data/map_2015_12_10_13_27'
+imsize = 20
+graphene_threshold = 0.0063
 light_threshold = -1
-heavy_threshold = 0.01
-dirt_border = 50
-minimum_graphene_area = 0.0
+heavy_threshold = 0.0226
+dirt_border = 30
+minimum_graphene_area = 0.4
 minimum_number_peaks = 2
 maximum_number_peaks = 12
-only_process_this_number_of_images = 4 # -1 all
+only_process_this_number_of_images = 60 # -1 all
 only_process_images_of_shape = None #(1024, 1024) # None or tuple
 remove_left_edge_number_pixels = -1 # -1 nothing to remove
 save_fft = True
 # Should electron counting be done
 calculate_actual_counts = True
 # Should we also save electron counted images when there are no peaks found
-always_save_images = True
-#parameters for electron counting
-baseline = 0.0
-countlevel = 0.0125
-peakwidth = 0.61
+always_save_images = False
+# parameters for electron counting
+baseline = 0.001
+countlevel = 0.11
+peakwidth = 0.654
+# only integrate electron signal and do not convert to counts
+only_integrate = False
 #######################################################################################################################
 #######################################################################################################################
 #######################################################################################################################
@@ -286,7 +291,8 @@ def subframes_preprocessing(filename, dirname, imsize, counts_threshold=1e-9, gr
             image = np.asarray(np.rint(image/counts_divisor), dtype='uint16')
         else:
         # New version of calculating counts
-            image = c_electron_counting.electron_counting(image, baseline=baseline, countlevel=countlevel, peakwidth=peakwidth)
+            image = c_electron_counting.electron_counting(image, baseline=baseline, countlevel=countlevel,
+                                                          peakwidth=peakwidth, only_integrate=only_integrate)
         #dilate mask if dirt_border > 0
     #    if dirt_border > 0:
     #        mask = cv2.dilate(mask, np.ones((dirt_border, dirt_border)))
@@ -309,7 +315,7 @@ def subframes_preprocessing(filename, dirname, imsize, counts_threshold=1e-9, gr
         if save_fft and success:
             #fft = np.log(np.abs(np.fft.fftshift(np.fft.fft2(image_org)))).astype('float32')
             fft = np.log(np.abs(Peak.fft)).astype(np.float32)
-            center = np.array(np.shape(image))/2
+            center = np.array(np.shape(image), dtype=np.int)/2
             ell = np.ones(np.shape(fft), dtype='float32')
             if np.mean(fft) > 0:
                 ellipse_color = 1.5
@@ -364,58 +370,58 @@ if __name__ == '__main__':
         if not os.path.exists(dirpath+'fft_'+dirpath.split('/')[-2]+'/'):
             os.makedirs(dirpath+'fft_'+dirpath.split('/')[-2]+'/')
 
-    try:
-        pool = Pool()
-        res = [pool.apply_async(subframes_preprocessing, (filename, dirpath, imsize),
-                                {'graphene_threshold': graphene_threshold, 'light_threshold': light_threshold,
-                                 'heavy_threshold': heavy_threshold, 'dirt_border':dirt_border, 'median_blur_diameter': 67,
-                                 'gaussian_blur_radius': 4, 'save_fft': save_fft, 'counts_divisor': 4.0690098e-05,
-                                 'minimum_graphene_area': minimum_graphene_area, 'minimum_number_peaks': minimum_number_peaks,
-                                 'baseline': baseline, 'countlevel': countlevel, 'peakwidth': peakwidth,
-                                 'calculate_actual_counts': calculate_actual_counts
-                                 }) for filename in matched_dirlist[0:only_process_this_number_of_images if
-                                                                    only_process_this_number_of_images > 0 else None]]
-        res_list = [p.get() for p in res]
-        pool.close()
-        pool.terminate()
 
-        #duration = time.time()-starttime
+    pool = Pool()
+    res = [pool.apply_async(subframes_preprocessing, (filename, dirpath, imsize),
+                            {'graphene_threshold': graphene_threshold, 'light_threshold': light_threshold,
+                             'heavy_threshold': heavy_threshold, 'dirt_border':dirt_border, 'median_blur_diameter': 67,
+                             'gaussian_blur_radius': 4, 'save_fft': save_fft, 'counts_divisor': 4.0690098e-05,
+                             'minimum_graphene_area': minimum_graphene_area, 'minimum_number_peaks': minimum_number_peaks,
+                             'baseline': baseline, 'countlevel': countlevel, 'peakwidth': peakwidth,
+                             'calculate_actual_counts': calculate_actual_counts
+                             }) for filename in matched_dirlist[0:only_process_this_number_of_images if
+                                                                only_process_this_number_of_images > 0 else None]]
+    res_list = [p.get() for p in res]
+    pool.close()
+    pool.terminate()
 
-        #print('Time for calculation: %.2f s' %(duration,))
+    #duration = time.time()-starttime
 
-        res_list.sort()
-    finally:
-        if not os.path.exists(dirpath+'prep_'+dirpath.split('/')[-2]+'/'):
-            os.makedirs(dirpath+'prep_'+dirpath.split('/')[-2]+'/')
+    #print('Time for calculation: %.2f s' %(duration,))
 
-        frame_data_file = open(dirpath+'prep_'+dirpath.split('/')[-2]+'/'+'frame_init_' + dirpath.split('/')[-2] + '.txt',
-                               'w')
+    res_list.sort()
 
-        frame_data_file.write('#Informations about all frames of '+(dirpath.split('/')[-2]+'\n'))
-        frame_data_file.write('#Created: ' + time.strftime('%Y/%m/%d %H:%M') + '\n')
-        frame_data_file.write('#Imagesize in nm: {:.1f}\tgraphene threshold: {:f}\t'.format(imsize,graphene_threshold))
-        frame_data_file.write('light threshold: {:f}\theavy threshold: {:f}\t'.format(light_threshold, heavy_threshold))
-        frame_data_file.write('minimum number peaks: {:.0f}\t'.format(minimum_number_peaks))
-        frame_data_file.write('Dirt border: {:n}\tminimum graphene area: {:f}\n'.format(dirt_border, minimum_graphene_area))
-        if calculate_actual_counts:
-            frame_data_file.write('#Baseline: {:f}\tcountlevel: {:f}\tpeakwidth: {:n}\n'.format(baseline, countlevel, peakwidth))
-        if os.path.isfile(os.path.join(dirpath, 'map_info.txt')):
-            with open(os.path.join(dirpath, 'map_info.txt')) as infofile:
-                for line in infofile:
-                    frame_data_file.write('#' + line)
-        frame_data_file.write('#label\tgraphene\tnumpeak\ttuning\ttilt\tella\tellb\tellphi\n\n')
+    if not os.path.exists(dirpath+'prep_'+dirpath.split('/')[-2]+'/'):
+        os.makedirs(dirpath+'prep_'+dirpath.split('/')[-2]+'/')
 
-        for frame_data in res_list:
-            if frame_data[-1]:
-                    frame_data_file.write('%s\t%.3f\t%d\t%.2f\t%.6f\t%.6f\t%.6f\t%.6f\n' % frame_data[0:-1])
+    frame_data_file = open(dirpath+'prep_'+dirpath.split('/')[-2]+'/'+'frame_init_' + dirpath.split('/')[-2] + '.txt',
+                           'w')
 
-        frame_data_file.close()
+    frame_data_file.write('#Informations about all frames of '+(dirpath.split('/')[-2]+'\n'))
+    frame_data_file.write('#Created: ' + time.strftime('%Y/%m/%d %H:%M') + '\n')
+    frame_data_file.write('#Imagesize in nm: {:.1f}\tgraphene threshold: {:f}\t'.format(imsize,graphene_threshold))
+    frame_data_file.write('light threshold: {:f}\theavy threshold: {:f}\t'.format(light_threshold, heavy_threshold))
+    frame_data_file.write('minimum number peaks: {:.0f}\t'.format(minimum_number_peaks))
+    frame_data_file.write('Dirt border: {:n}\tminimum graphene area: {:f}\n'.format(dirt_border, minimum_graphene_area))
+    if calculate_actual_counts:
+        frame_data_file.write('#Baseline: {:f}\tcountlevel: {:f}\tpeakwidth: {:n}\n'.format(baseline, countlevel, peakwidth))
+    if os.path.isfile(os.path.join(dirpath, 'map_info.txt')):
+        with open(os.path.join(dirpath, 'map_info.txt')) as infofile:
+            for line in infofile:
+                frame_data_file.write('#' + line)
+    frame_data_file.write('#label\tgraphene\tnumpeak\ttuning\ttilt\tella\tellb\tellphi\n\n')
 
-        overall_time = time.time() - overall_starttime
+    for frame_data in res_list:
+        if frame_data[-1]:
+                frame_data_file.write('%s\t%.3f\t%d\t%.2f\t%.6f\t%.6f\t%.6f\t%.6f\n' % frame_data[0:-1])
 
-        print('Done analysing %d files in %.2f s.' %(only_process_this_number_of_images if
-                                                     only_process_this_number_of_images > 0 else
-                                                     len(matched_dirlist), overall_time))
+    frame_data_file.close()
+
+    overall_time = time.time() - overall_starttime
+
+    print('Done analysing %d files in %.2f s.' %(only_process_this_number_of_images if
+                                                 only_process_this_number_of_images > 0 else
+                                                 len(matched_dirlist), overall_time))
 
 #    res_list = []
 #    for name in matched_dirlist:
